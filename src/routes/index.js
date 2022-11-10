0702b582153695c1343bf23ec396cd46ffba43e3
taturo2001 <92012553+taturo2001@users.noreply.github.com>
2022-11-10 14:02:50 +0700

    updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
 
 export const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
 ];
 
 export const privateRoutes = [];
